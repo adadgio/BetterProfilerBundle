4e6cca8b538c40a6659e795f87f9ca6c4eb8011e
adadgio <romain.bruckert.esc@gmail.com>
2017-04-07 15:05:32 +0200

    Second commit.

--- Service/DemoService.php
+++ Service/DemoService.php
@@ -16,7 +16,7 @@ class DemoService extends CollectableService implements CollectableServiceInterf
     public function doSomething()
     {
         $event = (new CollectableEvent('1st event'))->start();
-        sleep(1.7);
+        // sleep(1.7);
 
         $event->stop();
         $this->addCollectedEvent($event);
@@ -25,8 +25,8 @@ class DemoService extends CollectableService implements CollectableServiceInterf
     public function doAnotherThing()
     {
         $event = (new CollectableEvent('2nd event'))->start();
-        sleep(1.67);
-        
+        // sleep(1.67);
+
         $event->stop();
         $event->addTrace('');
         $this->addCollectedEvent($event);
